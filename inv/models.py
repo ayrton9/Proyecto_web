6a6fa081da9740110182f4553cdcc06f76351967
Eduardo <eruizg1@miumg.edu.gt>
2020-10-29 14:35:24 -0600

    Contrccion de Opciones de Catalogo

--- inv/models.py
+++ inv/models.py
@@ -19,3 +19,60 @@ class Categoria(ClaseModelo):
 
     class Meta:
         verbose_name_plural= "Categorias"
+
+
+class SubCategoria(ClaseModelo):
+    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE)
+    descripcion = models.CharField(
+        max_length=100,
+        help_text= 'Descripcion de  la Categoria',
+        
+    )
+
+    def __str__(self):
+        return '{}:{}'.format(self.categoria.descripcion,self.descripcion)
+    
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(SubCategoria,self).save()
+    
+    class Meta:
+        verbose_name_plural= "Sub Categorias"
+        unique_together = ('categoria','descripcion')
+
+
+class Marca(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripcion de la Marca',
+        unique=True
+    )
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+    
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Marca, self).save()
+    
+    class Meta:
+        verbose_name_plural = "Marca"
+
+
+class UnidadMeidida(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripcion de la Unidad Medida',
+        unique=True
+    )
+    def __str__(self):
+        return '{}'.format(self.descripcion)
+    
+    def save(self):
+        self.descripcion = self.descripcion.upper()
+        super(UnidadMeidida, self).save()
+    
+    class Meta:
+        verbose_name_plural = "Unidades de Medida"
+
+
+
